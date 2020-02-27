a4d63f0c9a1c7c70bc326f0f6e8cbed32b4a48b2
Kendall Nakai <kendallnakai@gmail.com>
2020-02-27 08:28:08 -0800

    Add rsvp function

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,13 @@ var data = {
 /*
  * GET tasks page.
  */
+ exports.addRSVP = function(req, res) {
+ 	var rsvpEmail = req.body.rsvpEmail;
+ 	console.log(rsvpEmail);
+ 	data.rsvp.push(rsvpEmail);
+ 	res.send(rsvpEmail);
+ }
+
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
